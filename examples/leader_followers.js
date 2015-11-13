c67b2a4e6ecc254f2a440858ff3bd6c8ed614a0e
Mike Lanzetta <milanz@microsoft.com>
2015-11-12 17:14:02 -0800

    Add example for leader/follower based on blob lease manager.

---
+++ examples/leader_followers.js
@@ -0,0 +1,32 @@
+var cerulean = require('./../lib/index');
+var LM = cerulean.LeaseManager;
+
+var accountName = '<redacted>';
+var accountKey = '<redacted>';
+var container = 'locks';
+var blob = 'leadertest';
+
+var name = process.argv[2];
+var timeToWait = process.argv[3];
+if (!name) throw new Error('Must provide name');
+if (!timeToWait) throw new Error('Must provide number of seconds to wait');
+
+timeToWait = 1000 * parseInt(timeToWait);
+
+var lease = new cerulean.Lease(accountName, accountKey, container, blob);
+var lm = new LM();
+lm.on(LM.Acquired, function() {
+    console.log('Lease acquired by ' + name);
+    lease.updateContents(JSON.stringify({ leader: name, acquired_on: new Date().toISOString() }));
+}).on(LM.Lost, function() {
+    console.log('Lease lost by ' + name);
+}).on(LM.Released, function() {
+    console.log('Lease released by '+name);
+});
+
+setTimeout(function() {
+    console.log(name + ' waited ' + (timeToWait/1000) + ' seconds. Releasing lease.');
+    lm.unmanageLease(lease);
+}, timeToWait);
+
+lm.manageLease(lease);
